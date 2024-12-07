994c2b7d80f6a4f4215cbee3829e485da6120832
Dapunta <dapuntaxd@gmail.com>
2024-12-08 02:27:29 +0700

    fix bug

--- backend/python/app/admin/edit_order.py
+++ backend/python/app/admin/edit_order.py
@@ -1,25 +1,4 @@
-import json
-import mysql.connector
-
-#--> Load MySQL config from JSON
-def load_mysql_config():
-    with open('backend/database/mysql_config.json') as f:
-        return json.load(f)
-
-#--> Connect to MySQL Database
-def get_db_connection():
-    config = load_mysql_config()
-    connection = mysql.connector.connect(
-        host=config['host'],
-        user=config['user'],
-        password=config['password'],
-        database=config['database'],
-        port=config['port'],
-        charset=config['charset']
-    )
-    return connection
-
-# ------------------------------
+from ..utils.connect_db import get_db_connection
 
 #--> Menghapus Data Pesanan Berdasar id_pesanan dan Belum Diproses
 def delete_order_by_id(id_pesanan):
